e16040769c3829a7b8d396fc74cb733193826d78
DanielAlk <daniodfad@yahoo.com.ar>
2016-01-14 18:33:41 -0300

    tinymce bug fix

--- app/helpers/admins_helper.rb
+++ app/helpers/admins_helper.rb
@@ -1,5 +1,10 @@
 module AdminsHelper
 	def tinymce_init
+		content_for :extra_js do
+			"setTimeout(function() {
+				if (!$('.tinymce').prev().is('.mce-panel')) window.location.reload();
+			}, 500);".html_safe
+		end
 		'<script>
 			//<![CDATA[
 			tinyMCE.init({
