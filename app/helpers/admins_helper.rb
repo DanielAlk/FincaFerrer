fcebe7a1382ec907c81011086c18c1fbc9b07dd7
DanielAlk <daniodfad@yahoo.com.ar>
2016-01-14 18:21:42 -0300

    fix some strange error with tinymce

--- app/helpers/admins_helper.rb
+++ app/helpers/admins_helper.rb
@@ -1,2 +1,16 @@
 module AdminsHelper
+	def tinymce_init
+		'<script>
+			//<![CDATA[
+			tinyMCE.init({
+				selector: "textarea.tinymce",
+				toolbar: "bold italic | undo redo | link",
+				menubar: false,
+				statusbar: false,
+				forced_root_block: false,
+				plugins: "link"
+			});
+			//]]>
+		</script>'.html_safe
+	end
 end
